9e3b6dcd692cd73095ff170ae18a1ccd2423b460
yxhwxn <officeyh9819@gmail.com>
2025-01-22 19:50:50 +0900

    Feat: 방 입장 시 exception handling 추가

---
+++ src/main/java/com/woori/codeshare/room/exception/RoomErrorCode.java
@@ -0,0 +1,24 @@
+package com.woori.codeshare.room.exception;
+
+import com.woori.codeshare.global.exception.BaseErrorCode;
+import com.woori.codeshare.global.response.ErrorResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum RoomErrorCode implements BaseErrorCode {
+
+    INVALID_PASSWORD(HttpStatus.FORBIDDEN, "ROOM403", "방 비밀번호가 잘못 되었습니다."),
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "ROOM404", "존재하지 않는 방입니다.");
+
+    private final HttpStatus status;  // HTTP 상태 코드
+    private final String code;        // 에러 코드
+    private final String message;     // 에러 메시지
+
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return ErrorResponse.of(code, message);
+    }
+}
