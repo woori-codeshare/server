cb1cf838d6186bfb6bcde288eb07997446619049
yxhwxn <officeyh9819@gmail.com>
2025-01-22 19:50:23 +0900

    Feat: 방 입장 API 구현

--- src/main/java/com/woori/codeshare/room/controller/dto/RoomResponseDTO.java
+++ src/main/java/com/woori/codeshare/room/controller/dto/RoomResponseDTO.java
@@ -2,6 +2,8 @@ package com.woori.codeshare.room.controller.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE) // 외부에서 인스턴스화 방지
 public class RoomResponseDTO {
 
@@ -13,5 +15,17 @@ public class RoomResponseDTO {
         private Long roomId;    // 방 ID
         private String uuid;    // 고유 식별자
         private String title;   // 방 제목
+        private LocalDateTime createdAt; // 방 생성 시각
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RoomEnterResponse {
+        private Long roomId;    // 방 ID
+        private String uuid;    // 방 고유 식별자
+        private String title;   // 방 제목
+        private LocalDateTime createdAt; // 방 생성 시각
     }
 }
--- src/main/java/com/woori/codeshare/room/service/RoomService.java
+++ src/main/java/com/woori/codeshare/room/service/RoomService.java
@@ -4,6 +4,8 @@ package com.woori.codeshare.room.service;
 import com.woori.codeshare.room.controller.dto.RoomRequestDTO;
 import com.woori.codeshare.room.controller.dto.RoomResponseDTO;
 import com.woori.codeshare.room.domain.Room;
+import com.woori.codeshare.room.exception.RoomErrorCode;
+import com.woori.codeshare.room.exception.RoomException;
 import com.woori.codeshare.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -43,6 +45,30 @@ public class RoomService {
                 .roomId(savedRoom.getRoomId())
                 .uuid(savedRoom.getUuid())
                 .title(savedRoom.getTitle())
+                .createdAt(savedRoom.getCreatedAt())
+                .build();
+    }
+
+    /**
+     * 방 입장 로직
+     *
+     * @param roomId      방 ID
+     * @param rawPassword 사용자 입력 비밀번호
+     * @return 방 입장 응답 DTO
+     */
+    public RoomResponseDTO.RoomEnterResponse enterRoom(Long roomId, String rawPassword) {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new RoomException(RoomErrorCode.ROOM_NOT_FOUND));
+
+        if (!passwordEncoder.matches(rawPassword, room.getPassword())) {
+            throw new RoomException(RoomErrorCode.INVALID_PASSWORD);
+        }
+
+        return RoomResponseDTO.RoomEnterResponse.builder()
+                .roomId(room.getRoomId())
+                .uuid(room.getUuid())
+                .title(room.getTitle())
+                .createdAt(room.getCreatedAt())
                 .build();
     }
 }
