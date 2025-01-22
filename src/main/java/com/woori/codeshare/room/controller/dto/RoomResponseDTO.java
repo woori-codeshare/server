f513d43d7d1923a6a54f7c609aad9fbac18ef214
yxhwxn <officeyh9819@gmail.com>
2025-01-22 17:50:59 +0900

    Feat: DTO, 빌더 패턴 적용

--- src/main/java/com/woori/codeshare/room/controller/dto/RoomResponseDTO.java
+++ src/main/java/com/woori/codeshare/room/controller/dto/RoomResponseDTO.java
@@ -1,15 +1,17 @@
 package com.woori.codeshare.room.controller.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE) // 외부에서 인스턴스화 방지
 public class RoomResponseDTO {
 
-    @Data
+    @Builder
+    @Getter
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class RoomCreateResponse {
-        private Long roomId;
-        private String uuid;
-        private String title;
+        private Long roomId;    // 방 ID
+        private String uuid;    // 고유 식별자
+        private String title;   // 방 제목
     }
 }
