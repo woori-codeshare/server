a757858ea91277588a5590aadd4ca4072040eaa9
yxhwxn <officeyh9819@gmail.com>
2025-02-20 17:02:00 +0900

    Feat: Payload 컨버터 추가 및 웹소켓 설정 수정

--- src/main/java/com/woori/codeshare/socket/config/WebSocketConfig.java
+++ src/main/java/com/woori/codeshare/socket/config/WebSocketConfig.java
@@ -1,22 +1,20 @@
 package com.woori.codeshare.socket.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+import java.util.List;
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 클라이언트가 WebSocket 서버에 연결할 엔드포인트 설정
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:63342", "http://localhost:8000")
-                .withSockJS();  // SockJS 사용 설정 (WebSocket 미지원 브라우저에서도 폴백)
-    }
+@Configuration
+@EnableWebSocket    // WebSocket 활성화
+@EnableWebSocketMessageBroker   // STOMP 활성화
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -25,4 +23,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 클라이언트가 서버로 메시지를 보낼 경로 설정
         registry.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 클라이언트가 WebSocket 서버에 연결할 엔드포인트 설정
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();  // SockJS 사용 설정 (WebSocket 미지원 브라우저에서도 폴백)
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> converters) {
+        converters.add(new MappingJackson2MessageConverter());
+        return false;  // 기본 메시지 컨버터를 유지
+    }
+
 }
