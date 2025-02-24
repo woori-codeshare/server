7989b51318fb20c4cfb81b469496d0314f541185
yxhwxn <officeyh9819@gmail.com>
2025-02-24 12:03:20 +0900

    Docs: 에러코드 추가 및 형식 최신화

--- src/main/java/com/woori/codeshare/global/exception/CommonErrorCode.java
+++ src/main/java/com/woori/codeshare/global/exception/CommonErrorCode.java
@@ -21,7 +21,7 @@ public enum CommonErrorCode implements BaseErrorCode {
     PAGE_NEGATIVE_INPUT(HttpStatus.BAD_REQUEST, "PAGE4001", "페이지 번호는 1이상의 숫자여야 합니다."),
 
     // JSON 관련 에러 추가
-    INVALID_JSON_FORMAT(HttpStatus.BAD_REQUEST, "JSON4001", "JSON 형식이 올바르지 않습니다. \" 기호가 포함된 경우 \\\" 로 이스케이프 처리하세요.");
+    INVALID_JSON_FORMAT(HttpStatus.BAD_REQUEST, "JSON1002", "JSON 형식이 올바르지 않습니다. \" 기호가 포함된 경우 \\\" 로 이스케이프 처리하세요.");
 
     private final HttpStatus httpStatus;
     private final String code;
